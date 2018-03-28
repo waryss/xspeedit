b26aef73479c780975d010985c0c527c4b62f5dd
waryss <warysswiser@gmail.com>
2018-03-28 09:00:06 +0200

    Make some conception and improve algo

---
+++ app/src/chain.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import Packet from "./packet";
+
+export default class Chain {
+
+    constructor() {
+        this.items = [];
+    }
+
+    get packages() {
+        return this.packets.join('/');
+    }
+
+    init(input) {
+        this.items = Array.from(input, c => parseInt(c)).filter(x => x > 0);
+        this.packets = [];
+    }
+
+    start() {
+        if (!this.items.length) {
+            throw new Error("Chain not initialized");
+        }
+        let currentPacket = new Packet();
+        this.packets.push(currentPacket);
+        while (this.items.length) {
+            let found = this.items.find(e => {
+                return currentPacket.canBeAdded(e);
+            });
+            if (found) {
+                currentPacket.addItem(found);
+                this.items.splice(this.items.indexOf(found), 1);
+            } else {
+                currentPacket = new Packet();
+                this.packets.push(currentPacket);
+            }
+        }
+    }
+}
--- app/src/robot.js
+++ app/src/robot.js
@@ -1,20 +1,16 @@
 'use strict'
 
+import Chain from "./chain";
+
 export default class Robot {
 
-  static process(items) {
-    let result = '';
-    let packetLength = 0;
-    for (let item of items) {
-      let itemLength = parseInt(item);
-      if( (packetLength+itemLength) <= 10) {
-        packetLength += itemLength;
-        result += item;
-      } else {
-        result += `/${item}`;
-        packetLength = itemLength
-      }
+    constructor() {
+        this.chain = new Chain();
     }
-    return result;
-  }
+
+    process(input) {
+        this.chain.init(input);
+        this.chain.start();
+    }
+
 }
